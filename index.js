c3f40b53f38e54958c23013808496c5558741cc5
iamrashidpathan <khanrshid11a@gmail.com>
2022-01-11 13:33:47 +0530

    complete dicee game

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.random()*6;
+randomNumber1 = Math.floor(randomNumber1)+1;
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
+
+var randomNumber2 = Math.random()*6;
+randomNumber2 = Math.floor(randomNumber2)+1;
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
+
+if(randomNumber1 == randomNumber2){
+    document.querySelector(".container h1").innerHTML="Draw!"
+}
+else if(randomNumber1 > randomNumber2){
+    document.querySelector(".container h1").innerHTML=" 🚩 Player 1 Wins!"
+}
+else{
+    document.querySelector(".container h1").innerHTML="Player 2 Wins! 🚩"
+}
